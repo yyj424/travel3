7b1e5aea4e73205683f22713c2053b1aa5b8ffb9
mango529 <leegoeun1218@naver.com>
2020-08-31 16:37:15 +0900

    Home

--- app/src/main/java/ddwucom/mobile/travel/HomeActivity.java
+++ app/src/main/java/ddwucom/mobile/travel/HomeActivity.java
@@ -1,12 +1,11 @@
 package ddwucom.mobile.travel;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,24 +13,23 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void onClick(View v) { // 충돌 위험 있으니 push는 하지 마삼!!
-        Intent intent;
         switch (v.getId()) { // 본인 필요한 부분만 주석 풀어서 쓰세욥.
 //            case R.id.btn_plan:
-//                intent = new Intent(this, );
-//                break;
+//                Intent intent = new Intent(this, );
+//                startActivity(intent);
 //            case R.id.btn_record:
-//                intent = new Intent(this, );
+//                Intent intent = new Intent(this, RecordListActivity.class);
+//                startActivity(intent);
 //                break;
 //            case R.id.btn_map:
-//                intent = new Intent(this, );
-//                break;
+//                Intent intent = new Intent(this, );
+//                startActivity(intent);
 //            case R.id.btn_course:
-//                intent = new Intent(this, );
-//                break;
+//                Intent intent = new Intent(this, );
+//                startActivity(intent);
 //            case R.id.btn_friends:
-//                intent = new Intent(this, );
-//                break;
+//                Intent intent = new Intent(this, );
+//                startActivity(intent);
         }
-        startActivity(intent);
     }
 }
