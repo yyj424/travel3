cad65cc4d2f200d39119d28c2284ab906531772b
hiereit <68723848+hiereit@users.noreply.github.com>
2020-10-28 20:00:27 +0900

    seraCommit 18 - some drawable images - edit searFriendsActivity interface - view searchFriends / activity edit

--- app/src/main/java/ddwucom/mobile/travel/SearchFriends.java
+++ app/src/main/java/ddwucom/mobile/travel/SearchFriends.java
@@ -2,11 +2,11 @@ package ddwucom.mobile.travel;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,15 +18,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.ObjectStreamField;
 import java.util.ArrayList;
 
 public class SearchFriends extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference dbRef;
 
-    ArrayAdapter<String> adapter;
     ArrayList<String> nicknameList;
+    ArrayList<String> arr;
 
+
+    MyFriendsAdapter myAdapter;
     ListView lvFriends;
     Button btn_addRoom;
     EditText etSearchFriends;
@@ -39,51 +42,53 @@ public class SearchFriends extends AppCompatActivity {
 
         database = FirebaseDatabase.getInstance();
         dbRef = database.getReference();
-//        nicknameList = new ArrayList<>();
-        //nicknameList = getNicknameArray();
-        Log.d("sera", nicknameList.toString());
 
+        nicknameList = new ArrayList<>();
+        nicknameList.add("sera");
+        nicknameList.add("yyj");
+        nicknameList.add("hiereit");
+        nicknameList.add("travel3Good");
+        //String[] nameList = getResources().getStringArray(R.array.nameList);
         btn_addRoom = findViewById(R.id.btn_make_room);
         etSearchFriends = findViewById(R.id.et_search);
+        //lvFriends.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
-        dbRef  = database.getReference("user_list");
+        Log.d("sera", "out of for-each" + arr.toString());
 
-        dbRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                nicknameList = new ArrayList<>();
-                Log.d("sera", "CLick22!!");
-                //String etNickname = etID.getText().toString();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Log.d("sera", "ValueEventListener : " + snapshot.child("nickname").getValue());
-                    //if(etNickname.equals(snapshot.child("nickname").getValue().toString())) {
-                    // Toast.makeText(JoinActivity.this, "중복된 아이디 입니다. 다시입력하세요 ", Toast.LENGTH_SHORT).show();
-                    nicknameList.add(snapshot.child("nickname").getValue().toString());
-                }
-
-                adapter = new ArrayAdapter<String>(SearchFriends.this, android.R.layout.simple_list_item_multiple_choice, nicknameList);
-                lvFriends = findViewById(R.id.lv_friends);
-                lvFriends.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-                lvFriends.setAdapter(adapter);
-                Log.d("sera", "for문 : " + nicknameList.toString());
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        //데이터 가져오기
-
-
-
-
-
-
+        myAdapter = new MyFriendsAdapter(SearchFriends.this, R.layout.search_friends_adpter, arr);
+        //myAdapter = new MyFriendsAdapter(SearchFriends.this, R.layout.search_friends_adpter, nameList);
+        lvFriends = findViewById(R.id.lv_friends);
+        lvFriends.setAdapter(myAdapter);
     }
-    public ArrayList getNicknameArray(){
-        final ArrayList<String> arr = new ArrayList<>();
-//        nicknameList = new ArrayList<>();
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.btn_search:
+                dbRef = database.getReference("user_list");
+                dbRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        arr = new ArrayList<>();
+                        Log.d("sera", "CLick22!!");
+                        //String etNickname = etID.getText().toString();
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            Log.d("sera", "ValueEventListener : " + snapshot.child("nickname").getValue());
+                            if (etSearchFriends.equals(snapshot.child("nickname").getValue().toString())) {
+                                arr.add(snapshot.child("nickname").getValue().toString());
+                                Log.d("sera", arr.toString());
+                            }
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
 
-        return arr;
+                    }
+                });
+                myAdapter = new MyFriendsAdapter(SearchFriends.this, R.layout.search_friends_adpter, arr);
+                //myAdapter = new MyFriendsAdapter(SearchFriends.this, R.layout.search_friends_adpter, nameList);
+                lvFriends = findViewById(R.id.lv_friends);
+                lvFriends.setAdapter(myAdapter);
+                break;
+        }
+
     }
 }
